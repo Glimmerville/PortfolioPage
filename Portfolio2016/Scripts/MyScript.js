da72ab2ed6b541a98da9b721e41bbacb09efd9cf
Your Name <you@email.com>
2016-05-24 15:15:04 -0400

    Added tabs that I do not think I will use

--- Portfolio2016/Scripts/MyScript.js
+++ Portfolio2016/Scripts/MyScript.js
@@ -11,3 +11,27 @@
             }, 2000);
         });
 }
+//I altered this below to do what I want, but it was originally found on js.fiddle.
+//http://jsfiddle.net/P8D22/17/
+// This is a shortcut for $(document).ready()
+$(function () {
+
+    // This is a shortcut to bind both mouseOver and mouseOut
+    $('div.slideOutTab').hover(function () {
+        // Animate out when hovered, stopping all previous animations
+        $(this)
+            .stop(true, false)
+            .animate({
+                left: -20
+            }, 350);
+    }, function () {
+        // Animate back in when not hovered, stopping all previous animations
+        $(this)
+            .stop(true, false)
+            .animate({
+                left: -400
+            }, 380);
+    });
+
+
+});
