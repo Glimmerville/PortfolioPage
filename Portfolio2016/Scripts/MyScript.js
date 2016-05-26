7874c83f0e80df52c89fd2f7a1a0f29ee5c96da4
Your Name <you@email.com>
2016-05-26 11:59:54 -0400

    Final WCCI Spring 2016 Portfolio Project

--- Portfolio2016/Scripts/MyScript.js
+++ Portfolio2016/Scripts/MyScript.js
@@ -1,20 +1,22 @@
-﻿function animationHover(element, animation) {
-    element = $(element);
-    element.hover(
-        function () {
-            element.addClass('animated ' + animation);
-        },
-        function () {
-            //wait for animation to finish before removing classes 
-            window.setTimeout(function () {
-                element.removeClass('animated ' + animation);
-            }, 2000);
-        });
-}
+﻿//None of this ended up being used because it was kind of distracting.
 
-$(document).ready(function () {
-    setTimeout(function () {
-        $('#frontlist').removeClass('hidden');
-    }, 1000);
-});
+//function animationHover(element, animation) {
+//    element = $(element);
+//    element.hover(
+//        function () {
+//            element.addClass('animated ' + animation);
+//        },
+//        function () {
+//            //wait for animation to finish before removing classes 
+//            window.setTimeout(function () {
+//                element.removeClass('animated ' + animation);
+//            }, 2000);
+//        });
+//}
+
+//$(document).ready(function () {
+//    setTimeout(function () {
+//        $('#frontlist').removeClass('hidden');
+//    }, 1000);
+//});
 
